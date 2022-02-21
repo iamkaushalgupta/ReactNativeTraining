147f2d0439ff855db8081588111e20df05b4ecbe
Kaushal Gupta <kaushal.gupta@successive.com>
2022-02-21 16:35:37 +0530

    After Seqence loop

--- App.tsx
+++ App.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { View } from "react-native";
- import MainStack from "./app/routes/stacknavigation";
-//import ReduxApp1 from "./app/redux";
+import MainStack from "./app/routes/stacknavigation";
 import Timing_animation from "./app/views/basics/timing";
 import Decay_animation from "./app/views/basics/decay";
 import Event_animation from "./app/views/basics/event";
 import FadeInOutOnPress from "./app/views/fadeInOutOnPress/fadeInOutOnPress";
-
-
+import Interpolation_demo from "./app/views/interpolation/interpolation";
+import Loop_demo from "./app/views/loop/loop";
+import Sequence_demo from "./app/views/seqence/sequence";
 const App=()=>
 {
   return(
-    <FadeInOutOnPress/>
+    <Sequence_demo/>
   );
 }
 
---
+++ app/views/seqence/sequence.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect } from "react";
+import {Text,View,Animated} from 'react-native'
+
+const Sequence_demo =()=>{
+    const animationDemo = new Animated.Value(0);
+    useEffect(()=>{
+        Animated.sequence([
+            Animated.timing(animationDemo,{
+                toValue: 285,
+                duration: 2000,
+               useNativeDriver: false
+            }),
+              Animated.timing(animationDemo, {
+                toValue: 10,
+                duration:2000,
+                useNativeDriver: false
+            }), ]
+          ).start()
+    },[])
+    return(
+        <View style={{flex:1, justifyContent:'center'}}>
+        <Animated.View 
+            style={{
+            height:100,
+            width:100,
+            borderRadius:90,
+            backgroundColor:animationDemo.interpolate({
+                inputRange:[0,100,180,285,],
+                outputRange:['red','green','yellow','blue']
+            }),
+
+            transform: [
+                { translateX: animationDemo },
+                { translateY: animationDemo.interpolate({
+                    inputRange:[0,80,100,180,285,],
+                    outputRange:[0,200,300,200,0],    
+                }) },
+            {scaleX:animationDemo.interpolate({
+                inputRange:[0,80,100,180,285,],
+                outputRange:[1,1.2,1.5,1.2,1],
+            })},
+            {scaleY:animationDemo.interpolate({
+                inputRange:[0,80,100,180,285,],
+                outputRange:[1,1.2,1.5,1.2,1],
+            })},
+                {rotate: animationDemo.interpolate({
+                    inputRange:[0,295],
+                    outputRange:['0deg','360deg']
+                })  }
+              ],
+              opacity:animationDemo.interpolate({
+            inputRange: [0,150,295],
+            outputRange: [0,1,0.5],
+
+})
+        }}
+        >
+
+        </Animated.View>
+    </View>
+    )
+}
+export default Sequence_demo;
