4b7dda8a748fcf09519ffffbc233065a632b092c
Kaushal Gupta <kaushal.gupta@successive.com>
2022-02-22 14:43:30 +0530

    After

--- App.tsx
+++ App.tsx
@@ -8,10 +8,11 @@ import Interpolation_demo from "./app/views/interpolation/interpolation";
 import Loop_demo from "./app/views/loop/loop";
 import Sequence_demo from "./app/views/seqence/sequence";
 import Parellel_demo from "./app/views/parallel/parallel";
+import Stagger_demo from "./app/views/stagger/stagger";
 const App=()=>
 {
   return(
-    <Parellel_demo/>
+    <Stagger_demo/>
   );
 }
 
--- app/views/loop/loop.tsx
+++ app/views/loop/loop.tsx
@@ -8,7 +8,7 @@ const Loop_demo =()=>{
               Animated.timing(animationDemo, {
              toValue: 285,
              duration: 2000,
-            useNativeDriver: true
+            useNativeDriver: false
            }),
             {
               iterations: 10
